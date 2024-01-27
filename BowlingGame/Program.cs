eeb77cb5aed060aad420ee1d03704a5b3aefd5db
successPhil <129565748+successPhil@users.noreply.github.com>
2024-01-26 22:20:01 -0500

    Look at all them passing tests

--- BowlingGame/Program.cs
+++ BowlingGame/Program.cs
@@ -2,19 +2,43 @@
 
 
 
-namespace BowlingGame
+namespace BowlingGameNS
 {
-    class BowlingGame
+    public class BowlingGame
     {
         public int score { get; private set; }
+        public int frame {get; private set;}
+
+        public int turns {get; private set;}
+
+        public int pins {get; private set;}
 
         public BowlingGame()
         {
             score = 0;
+            frame = 1;
+            pins = 10;
+            turns = 2;
         }
-        public void Roll(int pins)
-        {
-            score += pins;
+        public void Roll()
+        {   
+            //If turns == 0 Reset pins to 10 and increase frame by 1 and reset turns to two
+            if (turns == 0) {
+                pins = 10;
+                frame += 1;
+                turns = 2;
+            }
+
+
+            //If pins == 0 Reset pins to 10 and increase frame by 1** depending on spare/strike logic
+
+
+            Random random = new Random();
+            int randomRoll = random.Next(0, pins);
+            pins -= randomRoll;
+            score += randomRoll;
+            turns -= 1;
+            
         }
 
         // public int GetScore()
@@ -29,7 +53,7 @@ namespace BowlingGame
             Console.WriteLine("Get ready to lose in bowling there bucko!");
             BowlingGame game = new BowlingGame();
             Console.WriteLine(game.score);
-            game.Roll(4);
+            game.Roll();
             Console.WriteLine(game.score);
         }
     }
--- BowlingTests/UnitTest1.cs
+++ BowlingTests/UnitTest1.cs
@@ -1,10 +1,113 @@
-namespace BowlingTests;
+using BowlingGameNS;
 
-public class UnitTest1
+
+namespace BowlingTests
 {
-    [Fact]
-    public void Test1()
+    public class BowlingGameTests
     {
+        [Fact]
+        public void TestInitialCreation()
+        {
+            // Arrange
+            BowlingGame bg = new BowlingGame();
+
+            //Act
+            int expected = 0;
+            int actual = bg.score;
+            //Assert
+            Assert.Equal(expected, actual);
+            
+        }
+
+        [Fact]
+        public void TestInitialRoll()
+        {
+            // Arrange
+            BowlingGame bg = new BowlingGame();
+
+            // Act
+            bg.Roll();
+            int actual = bg.score;
+            int expected = 10 - bg.pins;
+
+            //Assert
+            Assert.Equal(expected, actual);
+            
+        }
+
+         [Fact]
+        public void TestInitialPins()
+        {
+            // Arrange
+            BowlingGame bg = new BowlingGame();
+
+            // Act
+            int actual = bg.pins;
+            int expected = 10;
+
+            //Assert
+            Assert.Equal(expected, actual);
+            
+        }
+
+          [Fact]
+        public void TestTwoRolls()
+        {
+            // Arrange
+            BowlingGame bg = new BowlingGame();
+
+            // Act
+            bg.Roll();
+            bg.Roll();
+            int actual = bg.score;
+            int expected = 10 - bg.pins;
+
+            //Assert
+            Assert.Equal(expected, actual);
+            
+        }
+
+        [Fact]
+
+        public void TestMultipleRandomRolls()
+        {
+            //Arrange
+            BowlingGame bg = new BowlingGame();
+
+            // Act
+            bg.Roll();
+            bg.Roll();
+            bg.Roll();
+            bg.Roll();
+            bg.Roll();
+            bg.Roll();
+            int actual = bg.frame;
+            int expected = 3;
+         
+
+
+            // Assert
+            Assert.Equal(actual, expected);
+        }
+
+        // [fact]
+
+        // public void TestSingleTurn()
+        // {
+        //     // Arrange
+        //     BowlingGame bg = new BowlingGame();
+
+        //     //Act
+
+        //     bg.Roll();
+        //     int expected = 1;
+        //     int actual = bg.turns;
+
+        //     // Assert
+        //     Assert.Equal(expected, actual);
+
+        // }
 
     }
+    
 }
