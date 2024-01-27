d9055800e74b682d33a42be816f5d5649dc6c56b
successPhil <129565748+successPhil@users.noreply.github.com>
2024-01-26 19:30:44 -0500

    first commit

---
+++ BowlingGame/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+
+namespace BowlingGame
+{
+    class BowlingGame
+    {
+        public int score { get; private set; }
+
+        public BowlingGame()
+        {
+            score = 0;
+        }
+        public void Roll(int pins)
+        {
+            score += pins;
+        }
+
+        // public int GetScore()
+        // {
+        //     return score;
+        // }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Get ready to lose in bowling there bucko!");
+            BowlingGame game = new BowlingGame();
+            Console.WriteLine(game.score);
+            game.Roll(4);
+            Console.WriteLine(game.score);
+        }
+    }
+}
+
---
+++ BowlingTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace BowlingTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
